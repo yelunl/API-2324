5530b3de5dbe8f9173cac2846d9d65187e05ba09
yelunl <yeliz.unlu@hva.nl>
2024-04-22 16:42:08 +0200

    web speech fix

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,46 +1,49 @@
 const outputDiv = document.getElementById('output');
 const startButton = document.querySelectorAll('.startButton');
+const micImg = document.querySelector('.startButton img');
 
 // Check if browser supports the SpeechRecognition API
 if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
   
-    // Set recognition options if needed
-    // recognition.continuous = true;
-    // recognition.interimResults = true;
+    recognition.continuous = true;
+    recognition.interimResults = true;
   
     // Event handler for when speech recognition starts
     recognition.onstart = () => {
-      outputDiv.textContent = 'Listening...';
+      console.log('Listening...');
     };
   
     // Event handler for when speech recognition ends
     recognition.onend = () => {
-      // outputDiv.textContent = 'Speech recognition ended.';
+      console.log('Speech recognition ended');
     };
   
     // Event handler for speech recognition results
     recognition.onresult = (event) => {
+      console.log('Speech recognition result');
       const transcript = event.results[0][0].transcript;
-      outputDiv.textContent = 'You said: ' + transcript;
+      searchSiblings.inputElement.value = transcript;
     };
   
     // Event handler for errors
     recognition.onerror = (event) => {
-      outputDiv.textContent = 'Error occurred: ' + event.error;
+      console.log(event)
     };
   
+    let searchSiblings;
+
     // Event listener for button click to start recognition
     startButton.forEach(item => {
-      item.addEventListener('click', () => {
+      item.addEventListener('click', (e) => {
+        searchSiblings = {inputElement: e.target.offsetParent.nextElementSibling}
         recognition.start();
+        console.log(recognition);
       });
 
     })
-  } else {
-    // outputDiv.textContent = 'Speech recognition not supported in this browser.';
-    
+  } else {    
     startButton.forEach(item => {
       item.hidden = true;
     })
@@ -59,7 +62,6 @@ if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
     };
   
     const butt = document.querySelector('#butt')
-    // console.log(window.location.href)
   
     butt.addEventListener('click', (e) => {
         navigator.share(shareData)
@@ -73,7 +75,6 @@ if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
   }
   
 
-
   // ///////////////////////////////////// //
   // little poster follow cursor animation //
   // ///////////////////////////////////// //
@@ -81,26 +82,9 @@ if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
 const image = document.querySelector('#detail .backdrop img');
 const info = document.querySelector('.info');
 
-// info.addEventListener("mousemove", (e) => {
-//   // image.style.objectPositionX = -e.offsetX + "px";
-//   // image.style.objectPositionY = -e.offsetY + "px";
-//   const height = window.innerHeight
-//   const width = window.innerWidth
-//   console.log(-e.offsetX)
-//   console.log(-e.offsetY)
-//   image.style.objectPosition = `${-e.offsetX / 8}px ${-e.offsetY / 8}px`;
-// });
-
 const posterImage = document.querySelector('#detail .poster img');
 
 document.addEventListener("mousemove", (e) => {
-  // image.style.objectPositionX = -e.offsetX + "px";
-  // image.style.objectPositionY = -e.offsetY + "px";
-  const height = window.innerHeight
-  const width = window.innerWidth
-  // console.log('X: ' + e.target)
-  // console.log('X: ' + -e.offsetX)
-  // console.log('Y: ' + -e.offsetY)
   posterImage.style.top = `${e.clientY / 80}px`;
   posterImage.style.left = `${e.clientX / 80}px`;
 });
